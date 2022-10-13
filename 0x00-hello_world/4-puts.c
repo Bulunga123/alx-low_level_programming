43c94f14ec3c9bb6554707e36e7e591ea4945ac3
Bulunga123 <bulungabongza19@gmail.com>
2022-10-13 05:19:41 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-/*#!/bin/bash*/
 #include <stdio.h>
-int main(voidi)
+/**
+ *main - Printing
+ *Return: for now zero
+ *
+ */
+
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle/n");
 	return (0);
