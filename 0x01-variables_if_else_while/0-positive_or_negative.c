d41d97e66dbca85fa5aa08cffe39df23a1621081
Bulunga123 <bulungabongza19@gmail.com>
2022-10-14 05:53:58 -0700

    task 1

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -print function
+ * Return: Always zero
+ *
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d and is greater than 5\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d and is 0\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d and is less than 6 and 0\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+coditinal statements
