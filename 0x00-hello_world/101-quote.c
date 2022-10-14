cbde3893e5588dc89ed1756c69992d5b7ac0fe25
Bulunga123 <bulungabongza19@gmail.com>
2022-10-13 18:35:13 -0700

    task last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
- * main - display
- * Return -return 1 for now
+ * main - Prints a text
+ * Return: Always return 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar,2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
