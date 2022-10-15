f398c247fedf24b8d1d59e634652f8033ac2cf3c
Bulunga123 <bulungabongza19@gmail.com>
2022-10-15 02:06:55 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,7 @@ int main(void)
 	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (int i = 0, i < 26, i++)
+	for (i = 0, i < 26, i++)
 	{putchar(alphabets[i];	}
 			putchar("\n");
 			return (0);
