0193ab9d0c6788197622e04c12fc2f0585a11deb
Bulunga123 <bulungabongza19@gmail.com>
2022-10-15 04:51:22 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print function
+ * Return: 0 for now
+ */
+int main(void)
+{
+	char a;
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+	return (0);
+}
