d28420b70ab2500d5f3746997126d95cb2eab8c3
Bulunga123 <bulungabongza19@gmail.com>
2022-10-15 04:21:22 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function print
+ * Return: function that return
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+		putchar(a);
+	putchar('\n);
+	return (0);
+}
