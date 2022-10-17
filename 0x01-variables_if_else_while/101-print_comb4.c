401b48dd6287d49dfcb2d68476e1ac52f85baa1a
Bulunga123 <bulungabongza19@gmail.com>
2022-10-17 02:20:44 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,10 +10,13 @@ int main(void)
 	int c;
 
 	for (a = 48; a < 58; a++)
+	{
 		for (b = 48; b < 58; b++)
+		{
 			for (c = 48; c < 58; c++)
 			{
 			if (a != b && a != c && a < b && a < c && b != c && b < c)
+			{
 				putchar(a);
 				putchar(b);
 				putchar(c);
@@ -22,6 +25,9 @@ int main(void)
 				putchar(',');
 				putchar(' ');
 			}
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
