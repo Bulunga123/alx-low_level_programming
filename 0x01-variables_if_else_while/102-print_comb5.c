c803aa9afff75f382c2c90c617fde044aceedf3b
Bulunga123 <bulungabongza19@gmail.com>
2022-10-17 02:52:18 -0700

    last task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ *main - functin
+ *Return: 0 fo now
+*/
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int v1;
+	int v2;
+
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 48; b < 58; b++)
+		{
+			for (c = 48 ;  c < 58; c++)
+			{
+				for (d = 48; d < 58; d++)
+				{
+					v1 = (a * 10) + b;
+					v2 = (c * 10) + d;
+					if (v1 < v2)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
+						if (a == 57 && b == 56 && c == 57 && d == 57)
+						{
+							break;
+						}
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
