ed928730bce47bc2b702310f34aa37a9f94d0fcf
Bulunga123 <bulungabongza19@gmail.com>
2022-10-18 06:48:03 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function display
+ * Return: nothin
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	char b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
+
