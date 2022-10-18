9f824b78322b48d269a21af9a861568056c6b39a
Bulunga123 <bulungabongza19@gmail.com>
2022-10-18 06:10:47 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include "main.h"
 /**
- *print_alphabet - display
+ *print_alphabet_x10 - display
  *Return: 0
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char a;
 
