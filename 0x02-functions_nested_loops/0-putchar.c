4b20159f3ce289855c387bf01742259920fedc09
Bulunga123 <bulungabongza19@gmail.com>
2022-10-18 04:55:00 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print - calls function
+ *Return: always 0
+ */
+int print(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
+
