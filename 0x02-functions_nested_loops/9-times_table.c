998e5d284999fc4b654b883cd62cfc23dd560382
Bulunga123 <bulungabongza19@gmail.com>
2022-10-19 04:25:13 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,45 +5,26 @@
  */
 void print_times_table(int n)
 {
-	int a, b, c;
+	int sum, a, b;
 
-	if (n >= 0 && n <= 15)
+	for (a = 0; a <= 9; a++)
 	{
-		for (a = 0; a <= n; a++)
+		_putchar('0');
+		for (b = 1; b <= 9; b++)
 		{
-			for (b = 0; b <= n; b++)
+			_putchar(',');
+			_putchar(' ');
+			sum = a * b;
+			if (sum <= 9)
 			{
-				 c = b * a;
-				 if (b == 0)
-				 {
-					 _putchar(c + '0');
-				 }
-				 else if (c < 10 && b != 0)
-				 {
-					 _putchar(',');
-					 _putchar(' ');
-					 _putchar(' ');
-					 _putchar(' ');
-					 _putchar(c + '0');
-				 }
-				 else if (c >= 10 && c < 10)
-				 {
-					 _putchar(',');
-					 _putchar(' ');
-					 _putchar(' ');
-					 _putchar((c / 10) + '0');
-					 _putchar((c % 10) + '0');
-				 }
-				 else if (c >= 100)
-				 {
-					 _putchar(',');
-					 _putchar(' ');
-					 _putchar((c / 100) + '0');
-					 _putchar((c / 10) % 10 + '0');
-					 _putchar((c % 10) + '0');
-				 }
+				_putchar(' ');
 			}
-			_putchar('\n');
+			else
+			{
+				_putchar((sum / 10) + '0');
+			}
+			_putchar((sum % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
