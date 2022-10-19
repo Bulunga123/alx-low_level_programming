1e34affa24f06e877b0f787ea3ec4f42d3da3948
Bulunga123 <bulungabongza19@gmail.com>
2022-10-18 17:03:32 -0700

    task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ *jack_bauer- function
+ *Return: return a nuumber
+ *
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			if (a < 10)
+			{
+				_putchar('0');
+				_putchar(a + '0');
+			}
+			else if (a >= 10)
+			{
+				_putchar((a % 10) + '0');
+				_putchar((a % 10) + '0');
+			}
+				if (b < 10)
+				{
+					_putchar(':');
+					_putchar('0');
+					_putchar(b);
+				}
+				else if (b > 10)
+				{
+					_putchar(':');
+					_putchar((b % 10) + '0');
+					_putchar((b % 10) + '0');
+				}
+			}
+		}
+	}
+}
