c5a478f3a57846df2c0383794863a69451a34f74
Bulunga123 <bulungabongza19@gmail.com>
2022-10-24 06:29:07 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *puts2 - function defination
+ *@str: parameter value
+ */
+void puts2(char *str)
+{
+	int a;
+
+	for (; str[a] != '\0'; a++)
+	{
+		if ((a % 2) == 0)
+		{
+			_putchar(str[a]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}
