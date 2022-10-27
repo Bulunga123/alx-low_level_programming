c8ddaf22f6c521bc21f42c1c5ffe11154999a557
Bulunga123 <bulungabongza19@gmail.com>
2022-10-27 03:40:31 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - function
+ * @a: param1
+ * @n: param2
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+}
